aec5933fd3dae0ab26892c03485662b98787242f
Marco <marco.steffens@gmail.com>
2013-11-11 22:42:31 +0100

    Init

---
+++ agentville-jade-archetype/src/main/java/agentville/jade/archetype/App.java
@@ -0,0 +1,13 @@
+package agentville.jade.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
