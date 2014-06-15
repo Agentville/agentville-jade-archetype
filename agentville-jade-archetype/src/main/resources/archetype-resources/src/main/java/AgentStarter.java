7032a5267f3f2c643186b36c02570b92f6651810
Marco <marco.steffens@gmail.com>
2014-06-15 10:00:59 +0200

    ...

--- agentville-jade-archetype/src/main/resources/archetype-resources/src/main/java/AgentStarter.java
+++ agentville-jade-archetype/src/main/resources/archetype-resources/src/main/java/AgentStarter.java
@@ -1,5 +1,8 @@
 package ${groupId};
 
+import java.util.logging.LogManager;
+
+import jade.core.Agent;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
@@ -7,7 +10,20 @@ import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.StaleProxyException;
 
+
+/**
+ * Die Implementierung der Main-Methode erzeugt einen lokalen 
+ * JADE-Main-Container mit RMA. Die damit erzeugte Plattform  
+ * ist die Testumgegung für die Entwicklung des Agenten und er-
+ * möglicht so eine kontrollierte Umgebung. Eine "produktive"
+ * Umgebung wird dadurch aber nicht erzeugt.
+ * 
+ * @author Marco Steffens
+ * 
+ */
 public class AgentStarter {
+	
+	Agent remoteManagementAgent = null;
 
 	public static void main(String[] args){
 	
@@ -19,6 +35,8 @@ public class AgentStarter {
 		host = "localhost";
 		port = -1;			//default-port 1099
 	
+		configureLogging();
+
 		Runtime runtime = Runtime.instance();
 	
 		Profile profile = null;
@@ -29,17 +47,17 @@ public class AgentStarter {
 		//Container erzeugen
 		container = runtime.createMainContainer(profile);
 
-//		Agent rmagent = new jade.tools.rma.rma();
-//		// Remote Monitoring Agenten erzeugen
-//		try {
-//			AgentController rma = container.acceptNewAgent(
-//	                	"RMA",
-//	                    rmagent
-//	                    );
-//			rma.start();
-//		} catch(StaleProxyException e) {
-//			throw new RuntimeException(e);
-//		}
+		remoteManagementAgent = new jade.tools.rma.rma();
+		// Remote Monitoring Agenten erzeugen
+		try {
+			AgentController rma = container.acceptNewAgent(
+	                	"RMA",
+	                	remoteManagementAgent
+	                    );
+			rma.start();
+		} catch(StaleProxyException e) {
+			throw new RuntimeException(e);
+		}
 	
 
 		// Agenten erzeugen und startet - oder aussteigen.
@@ -52,5 +70,34 @@ public class AgentStarter {
 	    } catch(StaleProxyException e) {
 	        throw new RuntimeException(e);
 	    }
+	}
+
+	/*
+	 * Das Logging-System von JADE basiert auf java.util.logging, und im
+	 * Zusammenhang mit Maven bringt das ein paar Probleme mit sich. Einmal
+	 * weil Java die Konfigurationsdatei per default im Installationsverzeichnis
+	 * des SDK sucht. Und dann noch weil der Logging-Agent von JADE nur ganz
+	 * am Anfang die Konfiguration einliest und sich nicht mehr umkonfigurieren
+	 * lässt. Das bedeutet: 
+	 * Da wir in einem Maven-Projekt alle relevanten Konfigurationsdateien im 
+	 * Projektverzeichnis ablegen wollen, müssen wir den Pfad dahin noch irgendwie
+	 * innerhalb unserer VM bekannt machen. Einer der üblichen Wege ist die Übergabe
+	 * als VM-Parameter beim Start, aber da wir unsere Agenten auf verschiedene Arten
+	 * starten wollen, bietet sich das nicht an. Da andererseites der Logging-Agent
+	 * die Konfiguration aber nur beim Start einliest, muss die Konfiguration VOR dem
+	 * Erzeugen der Plattform stattfinden. Und das ist genau hier. Das heißt aber auch,
+	 * dass bei allen Startarten, die die Main-Methode umgehen, die jeweils lokal
+	 * vorhandene Konfigurationsdatei verwendet wird.
+	 */
+	private static void configureLogging() {
+
+		System.setProperty( "java.util.logging.config.file",
+							"src/main/resources/logging.properties" );
+
+		try {
+			LogManager.getLogManager().readConfiguration();
+			}
+		catch ( Exception e ) { 
+			e.printStackTrace(); }
 	}
 }
